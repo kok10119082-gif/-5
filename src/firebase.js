f454a241f373e43c1fe6d3cb3bce69310e5e5ef6
kok10119082-gif <kok.10119082@gmail.com>
2025-11-18 19:56:59 +0900

    Initialize Firebase app and export auth and db

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,18 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "FIREBASE_API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
