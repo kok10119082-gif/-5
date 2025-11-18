3a225306cdc993620098e3a3f673ef3a50e6998b
kok10119082-gif <kok.10119082@gmail.com>
2025-11-18 20:05:14 +0900

    Add ElderDashboard component with child components

--- src/components/ElderDashboard.jsx
+++ src/components/ElderDashboard.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import ReminderEditor from "./ReminderEditor";
+import ReminderList from "./ReminderList";
+import RecordsTable from "./RecordsTable";
+import Chat from "./Chat";
 
+export default function ElderDashboard({ user }){
+  const elderId = user.uid;
+  return (
+    <div className="container">
+      <h2>ようこそ（高齢者画面）</h2>
+      <ReminderEditor userId={elderId} />
+      <ReminderList elderId={elderId} />
+      <RecordsTable elderId={elderId} />
+      <Chat elderId={elderId} myId={elderId} />
+    </div>
+  );
+}
