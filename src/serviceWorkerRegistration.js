02a9e993eec0e50a7b2691a419277a6c7d98fb19
kok10119082-gif <kok.10119082@gmail.com>
2025-11-18 20:40:25 +0900

    Implement service worker registration
    Add service worker registration function.

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,1 +1,8 @@
-
+// src/serviceWorkerRegistration.js
+export function register(){
+  if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('/sw.js').then(()=> {
+      console.log("SW registered");
+    }).catch(e=> console.log("SW error", e));
+  }
+}
