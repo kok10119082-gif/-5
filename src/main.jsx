ab67f0090b73793d139eb61436850c2dc17cf908
kok10119082-gif <kok.10119082@gmail.com>
2025-11-18 19:57:51 +0900

    Initialize React app and register service worker
    Set up React application with service worker registration.

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles.css";
+import { register } from "./serviceWorkerRegistration";
 
+createRoot(document.getElementById("root")).render(<App />);
+register(); // PWA service worker registration
